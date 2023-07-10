c536e78ee8a130a67ba636f609cd06aa0b7c5810
WeedWorm075 <weedWorm075@gmail.com>
2023-07-10 12:32:36 +0300

    sign in and sign up

---
+++ application/views/home.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<!-- Created by CodingLab |www.youtube.com/c/CodingLabYT-->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title> Exmen Mr Rojo</title>
+    <link rel="stylesheet" href="style.css">
+    <!-- Boxicons CDN Link -->
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link href="<?php echo base_url();?>/assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url();?>/assets/dist/css/home.css" rel="stylesheet">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+  <div class="sidebar">
+    <div class="logo-details">
+      <i class='bx bxl-c-plus-plus icon'></i>
+        <div class="logo_name">Examen S4</div>
+        <i class='bx bx-menu' id="btn" ></i>
+    </div>
+    <!-- <ul class="navs-list"> -->
+      <li>
+        <a href="#">
+          <i class='bx bx-home' ></i>
+          <span class="links_name">Home</span>
+        </a>
+         <span class="tooltip">Home</span>
+      </li>
+      <li>
+       <a href="#">
+         <i class='bx bx-user' ></i>
+         <span class="links_name">About us</span>
+       </a>
+       <span class="tooltip">About</span>
+     </li>
+     <li>
+       <a href="#">
+         <i class='bx bx-chat' ></i>
+         <span class="links_name">Contact us</span>
+       </a>
+       <span class="tooltip">Contact us</span>
+     </li>
+     <li>
+       <a href="#">
+         <i class='bx bx-folder' ></i>
+         <span class="links_name">Details</span>
+       </a>
+       <span class="tooltip">Detail</span>
+     </li>
+     <li>
+      <li class="profile" style="background-color: black;">
+         <div class="profile-details">
+           <div class="name_job">
+             <div class="name">Log out</div>
+             <div class="job">Examen Rojo</div>
+           </div>
+         </div>
+         <i class='bx bx-log-out' id="log_out" style="background-color: black;"></i>
+     </li>
+    </ul>
+  </div>
+  <!-- ato anatiny section no mapiditra anze zvt rht apidirina -->
+  <section class="home-section">
+    <nav class="bd-header bg-dark py-3 fixed d-flex align-items-stretch border-bottom border-dark">
+  <div class="container-fluid d-flex align-items-center">
+    <h1 class="d-flex align-items-center fs-4 text-white mb-0">
+      Examen Mr Rojo
+    </h1>
+  </div>
+</nav>
+    <div class="container">
+      <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+      <div class="text-center"><p class="h2" >ceci est un titre</p></div>
+      <div class="text-center">
+            <table class="table table-borderless">
+          <thead style="background-color:#0d6efd; color: white;">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+          </thead>
+          <tbody class="table-dark">
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          </tbody>
+        </table>
+        </div>
+        <div class="text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+  </section>
+  
+<footer class="footer mt-auto py-3 bg-dark">
+  <div class="container">
+    <span class="text-muted">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><div class="nav-link px-2 text-muted">Addresse: IT University, Antananarivo, Andoharanofotsy</div></li>
+      <li class="nav-item"><div class="nav-link px-2 text-muted">Contact: +261 12 345 67</div></li>
+      <li class="nav-item"><div class="nav-link px-2 text-muted">E-mail: Eemple@Gmail.Com</div></li>
+    </ul>
+    <p class="text-center text-muted">Examen S4 © 2023 IT University</p>
+  </span>
+  </div>
+</footer>
+
+  <script src="<?php echo base_url();?>/assets/dist/js/home.js"></script>
+
+</body>
+</html>
